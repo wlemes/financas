444ebcfc99beeb6a8c5d2be86b8de542d46ecce7
Wendel Lemes <wendellemes0@gmail.com>
2019-08-23 11:53:45 -0300

    Certo

---
+++ FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,12 @@
+package br.com.alura.financask.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.financask.R
+
+class ListaTransacoesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
